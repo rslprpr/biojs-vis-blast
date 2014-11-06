0b5758ebe0bfd31d038830f9d18dc85ba4f37858
Xavier Watkins <xavier.watkins@gmail.com>
2014-11-06 12:40:58 +0000

    Some stuff

--- snippets/simple_example.js
+++ snippets/simple_example.js
@@ -1,3 +1,7 @@
 // if you don't specify a html file, the sniper will generate a div
 var app = require("biojs-vis-blast");
-var instance = new app({el: yourDiv});
+var blast = require('biojs-io-blast');
+blast.read('../blast_results_ncbi.xml', function(jsonData){
+	var instance = new app({el: yourDiv, model: jsonData});
+	instance.render();
+});
