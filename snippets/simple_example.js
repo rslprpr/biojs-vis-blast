0b7b150dd219f4e38f879d7da3185fa9221d4689
Xavier Watkins <xavier.watkins@gmail.com>
2014-11-12 11:49:00 +0000

    Updated biojs-io version, switched to ebi blast, changed visualisation strategy

--- snippets/simple_example.js
+++ snippets/simple_example.js
@@ -1,7 +1,7 @@
 // if you don't specify a html file, the sniper will generate a div
 var app = require("biojs-vis-blast");
 var blast = require('biojs-io-blast');
-blast.read('../blast_results_ncbi.xml', function(jsonData){
+blast.read('../blast_results_ebi.xml', function(jsonData){
 	var instance = new app({el: yourDiv, model: jsonData});
 	instance.render();
 });
