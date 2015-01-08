aef4733da45fe64411d47311e3ed367f97966018
Sebastian Wilzbach <rasouli.homa@gmail.com>
2015-01-08 11:01:51 +0100

    changes

--- snippets/simple_example.js
+++
@@ -1,7 +0,0 @@
-// if you don't specify a html file, the sniper will generate a div
-var app = require("biojs-vis-blast");
-var blast = require('biojs-io-blast');
-blast.read('../blast_results_ebi.xml', function(jsonData){
-	var instance = new app({el: yourDiv, model: jsonData});
-	instance.render();
-});
